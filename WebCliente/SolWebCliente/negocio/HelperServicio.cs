b679f3a433cdbbe9c3bbdd84e3de6580cd3951fe
marlontaro <mcordova@soenperu.com>
2014-01-27 20:10:58 -0500

    web cliente 3 capas

---
+++ WebCliente/SolWebCliente/negocio/HelperServicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace it.negocio
+{
+    public class HelperServicio
+    {
+        public bool ValidarCorreo(string email)
+        {
+            string expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                { return true; }
+                else
+                { return false; }
+            }
+            else
+            { return false; }
+        }
+    }
+}
---
+++ WebCliente/SolWebCliente/negocio/ServicioException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace it.negocio
+{
+    public class ServicioException : Exception
+    {
+        public ServicioException()
+            : base()
+        {
+
+        }
+
+        public ServicioException(string mensaje)
+            : base(mensaje)
+        {
+
+        }
+
+        public ServicioException(string mensaje, Exception excepcionAnidada)
+            : base(mensaje, excepcionAnidada)
+        {
+
+        }
+    }
+}
